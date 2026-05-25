5b7d229eb6d5410a7f01260d216f63aafae150e7
evgenyk001 <ev.kozmenko@mail.ru>
2026-05-25 15:19:36 +1000

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/pulsedv/frontend/',
+  base: './',  // 👈 это важно для GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
